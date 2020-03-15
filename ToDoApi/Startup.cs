3c86b5215cb537032cc9021d97b2a34090968818
Neteia <b.brzykcy@gmail.com>
2020-03-15 12:28:58 +0100

    Added angular project

--- ToDoApi/Startup.cs
+++ ToDoApi/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using ToDoApi.Models;
 
 namespace ToDoApi {
     public class Startup {
@@ -21,6 +16,8 @@ namespace ToDoApi {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices( IServiceCollection services ) {
+            // services.AddDbContext<TodoContext>( opt => opt.UseInMemoryDatabase( "TodoList" ) );
+            services.AddDbContext<TodoContext>( opt => opt.UseSqlServer( Configuration.GetConnectionString( "TodoApiConnection" ) ) );
             services.AddControllers();
         }
 
